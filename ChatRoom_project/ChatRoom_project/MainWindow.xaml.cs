2da1f91c06ed68f138b425d3f38636a99c246c03
rotba <rotba@post.bgu.ac.il>
2018-04-26 17:04:27 +0300

    Crazy app

--- ChatRoom_project/ChatRoom_project/MainWindow.xaml.cs
+++ ChatRoom_project/ChatRoom_project/MainWindow.xaml.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using ConsoleApp1.BuissnessLayer;
+using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -20,9 +22,26 @@ namespace ChatRoom_project
     /// </summary>
     public partial class MainWindow : Window
     {
+        List<Message> items = new List<Message>();
+        int i = 0;
+        private ObservableCollection<Message> messages;
+        Dictionary<int, string> names = new Dictionary<int, string>()
+        {
+            { 0, "Tomer"},
+            { 1, "Dima"},
+            { 2, "Rotem"}
+        };
         public MainWindow()
         {
             InitializeComponent();
+            messages = new ObservableCollection<Message>();
+            lbMessages.ItemsSource = messages;
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            i = i % 3;
+            messages.Insert(messages.Count, new Message(new Guid(), names[i], DateTime.Now, "my name is " + names[i++], "15"));
         }
     }
 }
