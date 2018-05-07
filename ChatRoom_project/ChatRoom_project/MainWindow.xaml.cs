c5d1b6a0b3bc8d5929f1d8df99046e9f5517d458
arielr77 <arielron@post.bgu.ac.il>
2018-05-07 20:28:02 +0300

    Login & Register - MainWindow.xaml.cs + ObservableModelMainWindow.cs

--- ChatRoom_project/ChatRoom_project/MainWindow.xaml.cs
+++ ChatRoom_project/ChatRoom_project/MainWindow.xaml.cs
@@ -53,63 +53,31 @@ namespace ChatRoom_project
         }
 
         private void Button_Click(object sender, RoutedEventArgs e)
-        {
+        {/*
             verifyNickName(nicknameBox.Text);
             chtrm.login(g_IDToIntAndVerify(g_IDBox.Text), nicknameBox.Text);
             nicknameBox.Text = String.Empty;
+
             g_IDBox.Text = String.Empty;
             ChatRoomWindow chtrmWindow = new ChatRoomWindow(chtrm, this);
             chtrmWindow.Show();
-            this.Hide();
+            this.Hide();*/
+            _main.login(_main.G_IDBox, _main.NicknameBox);
         }
 
         private void Register_Click(object sender, RoutedEventArgs e)
         {
 
-          // verifyNickName(nicknameBox.Text);
-          //  chtrm.register(g_IDToIntAndVerify(g_IDBox.Text), nicknameBox.Text);
-          //  MessageBox.Show("Register completed successfully");
-          // nicknameBox.Text = String.Empty;
-          //  g_IDBox.Text = String.Empty;
+            // verifyNickName(nicknameBox.Text);
+            //  chtrm.register(g_IDToIntAndVerify(g_IDBox.Text), nicknameBox.Text);
+            //  MessageBox.Show("Register completed successfully");
+            // nicknameBox.Text = String.Empty;
+            //  g_IDBox.Text = String.Empty;
+            _main.register(_main.G_IDBox,_main.NicknameBox);
+
         }
 
 
-        /// <summary>
-        /// checks that a string is not emptry
-        /// </summary>
-        private bool verifyNickName(String nickName)
-        {
-            if (nickName == "")
-            {
-                log.Error("Attempted to enter empty nickname");
-                throw new ToUserException("NickName cannot be empty");
-            }
-            else
-                return true;
-        }
-
-        ///<summary>
-        ///checks if a string contains only the charcters '0'-'9' and converts it from string to int
-        ///<summary>
-        private int g_IDToIntAndVerify(String g_ID)
-        {
-            int result;
-            try
-            {
-                result = Convert.ToInt32(g_ID);
-                return result;
-            }
-            catch (OverflowException)
-            {
-                log.Error("Attempted to login with g_ID: " + g_ID + " that is outside the range of the Int32 type.");
-                throw new ToUserException("group id " + g_ID + " is invalid must be inside the range of -2,147,483,648 to 2,147,483,647.");
-            }
-            catch (FormatException)
-            {
-                log.Error("Attempted to enter g_ID: " + g_ID + " that is not only number.");
-                throw new ToUserException("The group id " + g_ID + " is not a valid group ID, must contain only numbers");
-
-            }
-        }
+      
     }
 }
--- ChatRoom_project/ChatRoom_project/ObservableModelMainWindow.cs
+++ ChatRoom_project/ChatRoom_project/ObservableModelMainWindow.cs
@@ -17,12 +17,13 @@ namespace ChatRoom_project
     public class ObservableModelMainWindow : INotifyPropertyChanged
     {
         public event PropertyChangedEventHandler PropertyChanged;
+        private static readonly log4net.ILog log = log4net.LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
 
         public ObservableModelMainWindow()
         {
-  
-              //  Messages.CollectionChanged += Messages_CollectionChanged;
-            
+
+            //   Messages.CollectionChanged += Messages_CollectionChanged;
+
         }
         public ObservableCollection<string> Messages { get; } = new ObservableCollection<string>();
 
@@ -37,6 +38,7 @@ namespace ChatRoom_project
             set
             {
                 nicknameBox = value;
+                OnPropertyChanged("NicknameBox");
             }
         }
 
@@ -50,12 +52,68 @@ namespace ChatRoom_project
             set
             {
                 g_IDBox = value;
+                OnPropertyChanged("G_IDBox");
             }
         }
 
+        public void register(String g_ID, String nickname) {
+             verifyNickName(nickname);
+             chtrm.register(g_IDToIntAndVerify(g_ID), nickname);
+        //   MessageBox.Show("Register completed successfully");
+             this.G_IDBox = "";
+             this.nicknameBox="";
+
+        }
+
+        public void login(String g_ID, String nickname)
+        {
+            verifyNickName(nickname);
+            chtrm.login(g_IDToIntAndVerify(g_ID), nickname);
+            //   MessageBox.Show("Register completed successfully");
+            this.G_IDBox = "";
+            this.nicknameBox = "";
+
+        }
         public void OnPropertyChanged([CallerMemberName] string propertyName = null)
         {
             PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+        /// <summary>
+        /// checks that a string is not emptry
+        /// </summary>
+        private bool verifyNickName(String nickName)
+        {
+            if (nickName == "")
+            {
+                log.Error("Attempted to enter empty nickname");
+                throw new ToUserException("NickName cannot be empty");
+            }
+            else
+                return true;
+        }
+
+        ///<summary>
+        ///checks if a string contains only the charcters '0'-'9' and converts it from string to int
+        ///<summary>
+        private int g_IDToIntAndVerify(String g_ID)
+        {
+            int result;
+            try
+            {
+                result = Convert.ToInt32(g_ID);
+                return result;
+            }
+            catch (OverflowException)
+            {
+                log.Error("Attempted to login with g_ID: " + g_ID + " that is outside the range of the Int32 type.");
+                throw new ToUserException("group id " + g_ID + " is invalid must be inside the range of -2,147,483,648 to 2,147,483,647.");
+            }
+            catch (FormatException)
+            {
+                log.Error("Attempted to enter g_ID: " + g_ID + " that is not only number.");
+                throw new ToUserException("The group id " + g_ID + " is not a valid group ID, must contain only numbers");
+
+            }
         }
 
     }
